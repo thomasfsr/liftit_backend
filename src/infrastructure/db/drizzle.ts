1bdd3099f4efdb3994991e6ce5e823cb73454f7b
Thomás Freire <thomas.fsr@gmail.com>
2026-02-20 15:46:34 -0300

    ok

--- src/infrastructure/db/drizzle.ts
+++ src/infrastructure/db/drizzle.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
+import * as schema from "./schemas/index";
 
 const connectionString = process.env.DATABASE_URL;
 
